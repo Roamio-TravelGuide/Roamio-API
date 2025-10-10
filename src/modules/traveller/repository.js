226d92f02285ea651ea52ec9a682fbe09ec0165b
Nijaani <140165189+Nijaani@users.noreply.github.com>
2025-10-10 13:01:05 +0530

    audio-play

--- src/modules/traveller/repository.js
+++ src/modules/traveller/repository.js
@@ -14,7 +14,50 @@ export class TravellerRepository {
         package_id: { not: null }
       },
       include: {
-        package: true
+        package: {
+          include: {
+            cover_image: {
+              select: {
+                url: true,
+              },
+            },
+            guide: {
+              include: {
+                user: {
+                  select: {
+                    name: true,
+                  },
+                },
+              },
+            },
+            tour_stops: {
+              include: {
+                location: true,
+                media: {
+                  include: {
+                    media: {
+                      select: {
+                        id: true,
+                        url: true,
+                        media_type: true,
+                        duration_seconds: true,
+                        file_size: true,
+                        format: true,
+                        bitrate: true,
+                        height: true,
+                        width: true,
+                        sample_rate: true
+                      }
+                    }
+                  }
+                }
+              },
+              orderBy: {
+                sequence_no: 'asc',
+              },
+            },
+          },
+        },
       },
       orderBy: {
         paid_at: 'asc' // or 'id': 'asc'
@@ -27,7 +70,15 @@ export class TravellerRepository {
     for (const payment of payments) {
       const pkg = payment.package;
       if (pkg && !seen.has(pkg.id)) {
-        uniquePackages.push(pkg);
+        // Flatten media for tour_stops
+        const transformedPkg = {
+          ...pkg,
+          tour_stops: pkg.tour_stops.map(stop => ({
+            ...stop,
+            media: stop.media.map(tsm => tsm.media)
+          }))
+        };
+        uniquePackages.push(transformedPkg);
         seen.add(pkg.id);
       }
     }
